5e584af5a5e62134fba0875c39dd287d4662282b
CynthiaMichelle9 <129108582+CynthiaMichelle9@users.noreply.github.com>
2023-06-27 20:09:56 -0400

    Actualización del proyecto
    - Codificación del modelo
    - Modificaciones en admin.py
    - Creación de nueva rama 'Individual2' para entregas posteriores.
    -No se han realizado las migraciones en espera de validaciónd del Awajker

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from django import forms
+from .models import CategoriaTarea
+from . import models
 
 # Register your models here.
+class CategoriaTareaAdmin(admin.ModelAdmin):
+    list_display = ['idetiqueta_tarea', 'nombre_categoria']
+    list_filter = ['nombre_categoria']
+    search_fields = ['nombre_categoria']
+
+formfield_overrides = {
+        models.CharField: {'widget': forms.Select(choices=CategoriaTarea.CATEGORIA_CHOICES)},
+    }
+admin.site.register(CategoriaTarea, CategoriaTareaAdmin)
--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+from django.db import models
+
+ESTADO_CHOICES = [
+        ('Pendiente', 'Pendiente'),
+        ('En progreso', 'En progreso'),
+        ('Completada', 'Completada'),
+    ]
+    
+CATEGORIA_CHOICES = [
+    ('Trabajo', 'Trabajo'),
+    ('Hogar', 'Hogar'),
+    ('Estudios', 'Estudios'),
+    ('Otros', 'Otros')
+]
+
+class EstadoTarea(models.Model):
+    idestado_tarea = models.IntegerField(primary_key=True)
+    estado_tarea = models.CharField(max_length=25, choices=ESTADO_CHOICES, default='Pendiente')
+
+class CategoriaTarea(models.Model):
+    idetiqueta_tarea = models.IntegerField(primary_key=True)
+    nombre_categoria = models.CharField(max_length=25, choices=CATEGORIA_CHOICES, default='Trabajo')
+
+
+class Tarea(models.Model):
+    idtarea = models.IntegerField(primary_key=True)
+    titulo = models.CharField(max_length=15)
+    descripcion = models.TextField()
+    fecha_vencimiento = models.DateTimeField()
+    deleted = models.BooleanField(default=False)
+    estado_tarea = models.ForeignKey(EstadoTarea, on_delete=models.CASCADE)
+    etiqueta_tarea = models.ForeignKey(CategoriaTarea, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def delete(self, *args, **kwargs):
+        self.deleted = True
+        self.save()
